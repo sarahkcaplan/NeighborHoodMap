adf6b3d11a6badbabc450762dbae2e8fe6f09acc
Sarah Caplan <caplan.sarah@gmail.com>
2017-09-01 13:01:26 -0700

    Add array for cats in model

---
+++ NeighborhoodMap/catclicker/js/catclicker.js
@@ -0,0 +1,82 @@
+$(function(){
+
+  var model = {
+    init: function(){
+      //not sure what to init
+    }
+  },
+  var catArray = [{
+    name: '',
+    clickCount: 0
+    imageUrl:
+  },
+  {
+    name: '',
+    clickCount: 0
+    imageUrl:
+  },
+  {
+    name: '',
+    clickCount: 0
+    imageUrl:
+  },
+  {
+    name: '',
+    clickCount: 0
+    imageUrl:
+  },
+  {
+    name: '',
+    clickCount: 0
+    imageUrl:
+  }
+  ]
+})
+
+
+
+//Array of objects. Objects are cat photo information
+[{
+  name: 'Cat',
+  clickCount: 0,
+  imageUrl:
+}
+]
+
+
+app loads
+create list
+list view populates with cats
+
+change current cat in model
+
+click on cat picture
+-track clicking
+-first in model
+-then in view
+
+click on list
+- change model
+-current cat reset
+-tell view to rerender
+
+
+view 1 = list of cats
+
+view 2 = cat details
+
+
+
+
+
+// //onClick() is a listener for click events registered using addEventListener()
+
+// function onClick() {
+//     var counter = document.getElementById("counter");
+//     counter.firstChild.nodeValue += 1;
+//   }
+
+
+// var el = document.getElementById("cat");
+// el.addEventListener("click", onClick);
+
