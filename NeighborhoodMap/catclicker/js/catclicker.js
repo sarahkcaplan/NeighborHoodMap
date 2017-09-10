b7ef4dc1eaa87099f543a165c78a45db32cb851b
Sarah Caplan <caplan.sarah@gmail.com>
2017-09-09 22:38:15 -0700

    Change counter to 1

--- NeighborhoodMap/catclicker/js/catclicker.js
+++ NeighborhoodMap/catclicker/js/catclicker.js
@@ -2,9 +2,13 @@ console.log("This script is running:) You got this, girl!")
 
 function catCount(){
   var photo = document.getElementById("clicker");
-  var count = document.getElementById("counter");
+  var counter = document.getElementById("counter");
   console.log("You clicked the kitty...")
-  count += 1
+  count = 0;
+  count += 1;
+  console.log("Count: ", count);
+  counter.innerHTML = count;
+  console.log("Count: ", count);
 }
 
 var photo = document.getElementById("clicker");
