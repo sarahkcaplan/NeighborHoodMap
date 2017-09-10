467fbdfab97a2c4f902304c51fddd95b28f4442c
Sarah Caplan <caplan.sarah@gmail.com>
2017-09-09 23:01:36 -0700

    Add correct catclicker.js file

--- NeighborhoodMap/catclicker/js/catclicker.js
+++ NeighborhoodMap/catclicker/js/catclicker.js
@@ -1,11 +1,35 @@
 
-function catCount(){
-  var photo = document.getElementById("clicker");
-  var counter = document.getElementById("counter");
-  count = parseInt(counter.textContent);
-  count += 1;
-  counter.innerHTML = count;
+function catCountBella(){
+  var photobella = document.getElementById("clicker-bella");
+  var counterbella = document.getElementById("counter-bella");
+  countbella = parseInt(counterbella.textContent);
+  countbella += 1;
+  counterbella.innerHTML = countbella;
 }
 
-var photo = document.getElementById("clicker");
-photo.addEventListener("click", catCount)
+var photobella = document.getElementById("clicker-bella");
+photobella.addEventListener("click", catCountBella);
+
+
+function catCountJack(){
+  var photojack = document.getElementById("clicker-jack");
+  var counterjack = document.getElementById("counter-jack");
+  countjack = parseInt(counterjack.textContent);
+  countjack += 1;
+  counterjack.innerHTML = countjack;
+}
+
+var photojack = document.getElementById("clicker-jack");
+photojack.addEventListener("click", catCountJack);
+
+
+function catCountCharles(){
+  var photocharles = document.getElementById("clicker-charles");
+  var countercharles = document.getElementById("counter-charles");
+  countcharles = parseInt(countercharles.textContent);
+  countcharles += 1;
+  countercharles.innerHTML = countcharles;
+}
+
+var photocharles = document.getElementById("clicker-charles");
+photocharles.addEventListener("click", catCountCharles);
