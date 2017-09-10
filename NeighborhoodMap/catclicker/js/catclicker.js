b8cf29915f4407f0d41ccf700d83962a9787ec2c
Sarah Caplan <caplan.sarah@gmail.com>
2017-09-09 22:46:29 -0700

    Increment count by 1 per click

--- NeighborhoodMap/catclicker/js/catclicker.js
+++ NeighborhoodMap/catclicker/js/catclicker.js
@@ -1,14 +1,10 @@
-console.log("This script is running:) You got this, girl!")
 
 function catCount(){
   var photo = document.getElementById("clicker");
   var counter = document.getElementById("counter");
-  console.log("You clicked the kitty...")
-  count = 0;
+  count = parseInt(counter.textContent);
   count += 1;
-  console.log("Count: ", count);
   counter.innerHTML = count;
-  console.log("Count: ", count);
 }
 
 var photo = document.getElementById("clicker");
