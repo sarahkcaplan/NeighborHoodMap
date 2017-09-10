c37982299508311c05996eb63beda31e5877a7b4
Sarah Caplan <caplan.sarah@gmail.com>
2017-09-09 22:27:01 -0700

    Add event listener for photo click

--- NeighborhoodMap/catclicker/js/catclicker.js
+++ NeighborhoodMap/catclicker/js/catclicker.js
@@ -1,1 +1,11 @@
 console.log("This script is running:) You got this, girl!")
+
+function catCount(){
+  var photo = document.getElementById("clicker");
+  var count = document.getElementById("counter");
+  console.log("You clicked the kitty...")
+  count += 1
+}
+
+var photo = document.getElementById("clicker");
+photo.addEventListener("click", catCount)
