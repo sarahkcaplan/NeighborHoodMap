65fd7f9e406d54041e18b9d2e53f1e982f94c842
Sarah Caplan <caplan.sarah@gmail.com>
2017-09-09 23:47:04 -0700

    Define majority of Octopus functions

--- NeighborhoodMap/catclicker/js/catclicker.js
+++ NeighborhoodMap/catclicker/js/catclicker.js
@@ -1,44 +1,49 @@
 // MODEL
+var currentCat = ''
 var catArray = [{
     name: 'Bella',
-    clickCount: 0
-    imageUrl:'images/bella.jpg'
+    clickCount: 0,
+    imageUrl: 'images/bella.jpg'
   },
   {
     name: 'Charles',
-    clickCount: 0
+    clickCount: 0,
     imageUrl: 'images/charles.jpg'
   },
   {
     name: 'Loretta',
-    clickCount: 0
+    clickCount: 0,
     imageUrl: 'images/jack.jpg'
   },
   {
     name: 'Tyler',
-    clickCount: 0
+    clickCount: 0,
     imageUrl:'images/Tyler.jpg'
   }
   ];
 
 // OCTOPUS
 function getListOfCats(){
-
+  catArray;
 }
 
 function getFirstCat(){
-
+  currentCat = catArray[0];
 }
 
-function getCatClickCount(){
+function getCatClickCount(currentCat){
 //might not need this one. might refactor and collapse into updateCatClickCount
+  catArray[currentCat].clickCount;
 }
 
-function updateCatClickCount(){
-
+function updateCatClickCount(currentCat){
+  var count document.getElementById("counter");
+  count = parseInt(counter.textContent);
+  count += 1;
+  catArray[currentCat].clickCount = count;
 }
 
-function getCat(){
+function getCurrentCat(){
 
 }
 
