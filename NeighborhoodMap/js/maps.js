b349e05247ddbed763b7e419b019e2660da683bc
Sarah Caplan <caplan.sarah@gmail.com>
2017-10-01 16:06:38 -0700

    Add map for Hong Kong centered on Kowloon

--- NeighborhoodMap/js/maps.js
+++ NeighborhoodMap/js/maps.js
@@ -1,4 +1,3 @@
-
 function initMap () {
   var lax = {lat: 33.9416, lng: -118.4085};
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -8,5 +7,13 @@ function initMap () {
   var marker = new google.maps.Marker({
     position: lax,
     map: map
+  });
+}
+
+function hongKong(){
+  var hk = {lat:22.3186, lng:114.1796}
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: hk
   });
 }
