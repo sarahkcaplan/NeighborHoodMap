f90e93fae73fe161a487eff2787f9b35f64f40e4
Sarah Caplan <caplan.sarah@gmail.com>
2017-10-01 16:31:33 -0700

    Add all locations to maps

--- NeighborhoodMap/js/maps.js
+++ NeighborhoodMap/js/maps.js
@@ -1,6 +1,11 @@
 var locations =[
 {lat: 33.9416, lng: -118.4085}, //lax
-{lat:22.3186, lng:114.1796} //hk
+{lat:22.3186, lng:114.1796}, //hk
+{lat: 18.7061, lng: 98.9817}, //chiang mai
+{lat: 19.0760, lng: 72.8777}, //mumbai
+{lat: -26.2041, lng: 28.0473}, //johannesberg
+{lat: -34.6037, lng: -58.3816}, //buenos aires
+{lat: 40.7128, lng: -74.0059} //nyc
 ]
 
 function initMap () {
